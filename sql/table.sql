fcef2b27b53da1b2c1b0bfbbd7d2132c6a08cf2f
shibez <ginta2888@gmail.com>
2023-04-16 14:43:14 +0700

    docs: Add database table design

---
+++ sql/table.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(50),
+    role VARCHAR(50),
+    password VARCHAR(100)
+);
+
+CREATE TABLE wallets (
+    id SERIAL PRIMARY KEY,
+    balance DECIMAL (10,2),
+    name VARCHAR(50),
+    user_id SERIAL,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    amount DECIMAL (10,2),
+    type VARCHAR(10),
+    category_id SERIAL,
+    wallet_id SERIAL,
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (wallet_id) REFERENCES wallets(id)
+);
